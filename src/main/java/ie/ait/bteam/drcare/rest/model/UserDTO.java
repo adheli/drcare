1aed7e5129dab12284133e605c569a7ddf586097
Jaohar Oseni <mekenjao@gmail.com>
2019-10-14 20:26:23 +0100

    - Getting ready for first user creation - Added validation to the UserDTO - Commented out the spring security dependency temporarily

--- src/main/java/ie/ait/bteam/drcare/rest/model/UserDTO.java
+++ src/main/java/ie/ait/bteam/drcare/rest/model/UserDTO.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotBlank;
+
 public class UserDTO implements Serializable {
 
 	@Getter
@@ -13,18 +15,22 @@ public class UserDTO implements Serializable {
 
 	@Getter
 	@Setter
+	@NotBlank(message = "Name should not be blank")
 	private String name;
 
 	@Getter
 	@Setter
+	@NotBlank(message = "username should not be blank")
 	private String username;
 
 	@Getter
 	@Setter
+	@NotBlank(message = "Password is required")
 	private String password;
 
 	@Getter
 	@Setter
+	@NotBlank(message = "Email is required")
 	private String email;
 
 	@Getter
