10c1d2a9dcb4815fa49109da14dba1207c02d6b4
Oluwaseun Odebunmi <seun_odebunmi@yahoo.com>
2019-10-30 23:55:39 +0000

    changed referenced dto in pharmacist rest service

--- src/main/java/ie/ait/bteam/drcare/rest/service/PharmacistRestService.java
+++ src/main/java/ie/ait/bteam/drcare/rest/service/PharmacistRestService.java
@@ -2,7 +2,7 @@ package ie.ait.bteam.drcare.rest.service;
 
 import ie.ait.bteam.drcare.data.model.User;
 import ie.ait.bteam.drcare.data.service.UserService;
-import ie.ait.bteam.drcare.rest.dto.PharmacistDTO;
+import ie.ait.bteam.drcare.rest.dto.UserDTO;
 import ie.ait.bteam.drcare.rest.translator.Translator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,15 +12,15 @@ import org.springframework.validation.BindingResult;
 public class PharmacistRestService {
 
     private UserService userService;
-    private Translator<User, PharmacistDTO> userTranslator;
+    private Translator<User, UserDTO> userTranslator;
 
     @Autowired
-    public PharmacistRestService(UserService userService, Translator<User, PharmacistDTO> userTranslator) {
+    public PharmacistRestService(UserService userService, Translator<User, UserDTO> userTranslator) {
         this.userService = userService;
         this.userTranslator = userTranslator;
     }
 
-    public PharmacistDTO createPharmacist(PharmacistDTO pharmacist, BindingResult result) {
+    public UserDTO createPharmacist(UserDTO pharmacist, BindingResult result) {
         User createdPharmacist = userTranslator.translateTo(pharmacist);
         createdPharmacist = userService.createUser(createdPharmacist, result);
 
