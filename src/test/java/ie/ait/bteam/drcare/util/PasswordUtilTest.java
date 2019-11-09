5567c3a4420fc9f7d869e88e33617ba031077df4
A00277158 <a00277158@student.ait.ie>
2019-11-09 20:40:30 +0000

    config

---
+++ src/test/java/ie/ait/bteam/drcare/util/PasswordUtilTest.java
@@ -0,0 +1,22 @@
+package ie.ait.bteam.drcare.util;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.junit.Assert.*;
+
+public class PasswordUtilTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    PasswordUtil newTest = new PasswordUtil();
+
+
+    @Test
+    public void encode() {
+
+    }
+}
